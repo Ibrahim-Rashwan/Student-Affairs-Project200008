2f9539cd7c4e8f051d7be66a6d7bec289a92284b
Elsayed-Ashraf <elgetmasayed@gmail.com>
2023-05-17 17:32:20 +0300

    haga helwaa

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Course extends Model
 {
     use HasFactory;
+    protected $fillable = [
+         'materials'
+    ];
+    protected function data(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => json_decode($value, true),
+            set: fn ($value) => json_encode($value),
+        );
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Doctorscontroller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,7 @@ Route::get('/', function () {
 Route::get('/sayed', function () {
     return view('welcome');
 });
+
+Route::resource('/Doctors',Doctorscontroller::class);
+
+//Route::get('/aa',[Doctorscontroller::class,'index']);
